a1763b00094fe918279f088abc6c9c996a0aa143
Shoichi Kaji <skaji@cpan.org>
2019-04-04 17:54:40 +0900

    hoge

--- 0407/main.go
+++ 0407/main.go
@@ -1,4 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
+	b := []byte("あいうえおabcかきくけこ")
+
+	r := []rune(string(b))
+
+	for i, j := 0, len(r)-1; i < j; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+
+	fmt.Println(string(r))
 }
