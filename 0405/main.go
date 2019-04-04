21570e34e4aa35c182a17789470b2dad83a9cae9
Shoichi Kaji <skaji@cpan.org>
2019-04-04 16:16:26 +0900

    update

--- 0405/main.go
+++ 0405/main.go
@@ -3,6 +3,9 @@ package main
 import "fmt"
 
 func squish(s []string) []string {
+	if len(s) == 0 {
+		return s
+	}
 	j := 0
 	for i := 0; i < len(s); i++ {
 		if s[j] == s[j+1] {
@@ -11,11 +14,12 @@ func squish(s []string) []string {
 			j++
 		}
 	}
-	return s[:j]
+	return s[:j+1]
 }
 
 func main() {
-	s := []string{"a", "b", "b", "b", "b", "c", "c", "d", "e", "a"}
+	squish([]string{})
+	s := []string{"a", "b", "b", "b", "b", "c", "c", "d", "e", "a", "a", "b"}
 	s = squish(s)
 	fmt.Println(s)
 }
